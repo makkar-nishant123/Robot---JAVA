e8b94afdebea9eb58fcda39240e5a9e1fbabe761
nmakkar@xavient.com <nmakkar@xavient.com>
2017-12-28 16:46:38 +0530

    First Commpit

---
+++ src/main/java/Robot/Robot_Art/Test.java
@@ -0,0 +1,13 @@
+package Robot.Robot_Art;
+
+public class Test {
+	
+	public void browserSetup() {
+	
+        System.setProperty("webdriver.chrome.driver", "d:\\s.w\\chromedriver.exe");
+    }
+	
+
+
+	
+}
